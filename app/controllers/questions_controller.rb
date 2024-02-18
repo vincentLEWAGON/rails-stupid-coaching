cdcbdcbe488026a848362a3e67595c4d824dfc25
Vincent Schmitt <vincentschmitt@free.fr>
2024-02-18 16:57:43 +0100

    exercice vérifié

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,15 @@
+
+# rails generate controller Questions
+
 class QuestionsController < ApplicationController
-  def ask
+  def ask # je créé une méthode ask pour la page d'accueil
   end
 
-  def answer
-    @question = params[:question]
-    case
-    when @question.include?('?')
+  def answer # je créé une méthode answer
+    @question = params[:question] # je récupère la question de l'utilisateur
+    case # je créé un case pour les différentes réponses
+      # attention à l'ordre des conditions qui doit être respecté
+    when @question.include?('?') # je vérifie si la question contient un point d'interrogation
       @answer = 'Silly question, get dressed and go to work!'
     when @question == @question.upcase
       @answer = "I can feel your motivation! I don't care, get dressed and go to work!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "ask", to: "questions#ask"
-  get "test", to: "questions#answer"
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "questions#ask" # je mets la page d'accueil sur la page ask
+
+  get "up" => "rails/health#show", as: :rails_health_check # je mets la page de santé sur la page up
+  get "ask", to: "questions#ask" # je créé la route ask sur la route ask
+  get "test", to: "questions#answer" # je créé la page answer sur la route test
+
 end
